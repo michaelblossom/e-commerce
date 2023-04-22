5231f860067808a3463c96480b0e674a4a72bd88
michaelblossom <michaelblossom8654@gmail.com>
2023-04-22 04:24:13 -0700

    Database connected

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" }); //to show the path that the config file is located
+
 const app = require("./app"); //importing app file to the server file
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log("Db connection successful");
+  });
 const port = 3000;
 // starting the server
 const server = app.listen(port, () => {
