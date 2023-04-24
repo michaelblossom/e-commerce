48ed2f5a67c96eb92df026285a124730fcc5d028
michaelblossom <michaelblossom8654@gmail.com>
2023-04-24 01:53:45 -0700

    moddeling user schema

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "please provide your email"],
+    unique: true,
+    lowercase: true,
+  },
+  photo: String,
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "please provide a password"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "passwords are not the same",
+    },
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
